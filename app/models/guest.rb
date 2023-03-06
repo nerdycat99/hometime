c7b93d10da60eb03704622c6acdde6f84a6398ed
nerdycat99 <info@drysky.co.uk>
2023-03-07 08:46:50 +1100

    models with validations

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -4,4 +4,7 @@ require 'json'
 
 class Guest < ApplicationRecord
   has_many :reservations, dependent: :destroy
+
+  validates :first_name, :last_name, :phone, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
